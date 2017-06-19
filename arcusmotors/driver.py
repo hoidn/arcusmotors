98c524cb8f9e51298944012b2de323996b24f38e
wholden <holdenwm523@gmail.com>
2017-06-19 12:26:12 -0700

    fixing initialize_motors

--- arcusmotors/driver.py
+++ arcusmotors/driver.py
@@ -1,6 +1,7 @@
 import ctypes
 from ctypes import cdll, POINTER, c_bool, c_void_p, c_long, c_int, byref
 from utils.utils import resource_path
+import time
 
 PERFORMAX_RETURN_SERIAL_NUMBER = 0x0
 PERFORMAX_RETURN_DESCRIPTION = 0x1
@@ -255,8 +256,8 @@ def initialize_motors():
     matches the entry in the global variable 'motordict'.  If not, raises
     an assertion error.
     """
-    motordict['camera']['handle'] = initialize_motor('camera')
-    motordict['sample']['handle'] = initialize_motor('sample')
+    initialize_motor('camera')
+    initialize_motor('sample')
 
 def camera_limit_minus():
     """Send the camera motor to the minus limit switch.
